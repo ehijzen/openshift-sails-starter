8e9870ccaeb038f54c9e8176fb890895a28a47c7
Yalim Aytul <yalim.aytul@gmail.com>
2015-10-01 14:17:35 -0500

    Additional changes for configuration to make sails work on OpenShift.

--- config/local.js
+++ config/local.js
@@ -3,6 +3,7 @@ module.exports = {
 
   host: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
   port: process.env.OPENSHIFT_NODEJS_PORT || 1337,
-  environment: process.env.NODE_ENV || 'development'
+  environment: process.env.NODE_ENV || 'development',
+  hookTimeout: 50000,
 
 };
